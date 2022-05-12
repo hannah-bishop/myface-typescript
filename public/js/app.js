d2351e6d1d9141e1ee3df75002c23d46b2a45d5e
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-12 11:30:41 +0100

    added button that randomises post background colour

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    
 });
+
+const button = document.getElementById("random-button");
+const postsArray = document.getElementsByClassName("profile-posts");
+let counter = 0;
+
+button.addEventListener("click", function() {
+    counter += 1;
+    console.log(counter);
+    for (values in postsArray) {
+        if (counter % 2 === 0) {
+            let randomColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+            postsArray[values].style["background-color"] = randomColor;
+        } else {
+            postsArray[values].style["background-color"] = "#151820";
+        }
+    }
+});
+
